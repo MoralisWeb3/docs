1812a52fd6b1b344ec18ffdcc68a27e908aaeafa
Koen Rijpstra <k.rijpstra@gmail.com>
2023-02-25 09:33:33 +0100

    Removed lambda-rate-limiter

--- api/exec.ts
+++ api/exec.ts
@@ -2,18 +2,8 @@ import { VercelRequest, VercelResponse } from "@vercel/node";
 import fetch from "node-fetch";
 import qs from "qs";
 import { Path } from "path-parser";
-const rateLimit = require("lambda-rate-limiter")({
-  interval: 60 * 1000 // Our rate-limit interval, one minute
-}).check;
 
 const exec = async (request: VercelRequest, response: VercelResponse) => {
-  try {
-    // 10 stands for the maximum amount of requests allowed during the defined interval
-    // rateLimit now returns a promise, let's await for it!
-    await rateLimit(10, request.headers["x-real-ip"]);
-  } catch (error) {
-    return { statusCode: 429 }; // Still returning a basic 429, but we could do anything~
-  }
   
   if (request.method !== "POST") {
     return response.status(405).json({ error: "Method Not Allowed" });
