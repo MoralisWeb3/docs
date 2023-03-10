2ffb227af96a188e543e43182a73229f293c24d1
Koen Rijpstra <k.rijpstra@gmail.com>
2023-03-10 10:00:46 +0100

    Added intercom setting + setup

--- src/plugins/docusaurus-plugin-segment/index.js
+++ src/plugins/docusaurus-plugin-segment/index.js
@@ -60,6 +60,16 @@ module.exports = function (context, fromOptions) {
             },
           },
           {
+            tagName: "script",
+            attributes: {
+              type: "text/plain",
+            },
+            innerHTML:
+              "window.intercomSettings={api_base:'https://api-iam.eu.intercom.io',app_id:'gy4tissf',};\n" +
+              "(function(){var w=window;var ic=w.Intercom;if(typeof ic==='function'){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/gy4tissf';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(document.readyState==='complete'){l();}else if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();" +
+              "\n",
+          },
+          {
             tagName: "link",
             attributes: {
               rel: "preconnect",
