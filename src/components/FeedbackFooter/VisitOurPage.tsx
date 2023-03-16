af0eb9e6fb399a61c897e61f9758834d99186ddf
Yoseph Kurnia Soenggoro <yosephkurniasoenggoro@gmail.com>
2023-03-16 17:49:32 +0700

    add segment analytics logic

--- src/components/FeedbackFooter/VisitOurPage.tsx
+++ src/components/FeedbackFooter/VisitOurPage.tsx
@@ -5,21 +5,23 @@ import BrowserOnly from "@docusaurus/BrowserOnly";
 const VisitOurPage = () => {
   return (
     <BrowserOnly>
-      {() => (
-        <div style={{ flex: 1 }}>
-          <h3>Need support?</h3>
-          <p>
-            Questions? Problems? Need more info? Contact Segment Support for
-            assistance!
-          </p>
-          <button
-            className={styles.visitOurButton}
-            onClick={() => window.open("https://help.moralis.io/", "_blank")}
-          >
-            Visit our Support Page
-          </button>
-        </div>
-      )}
+      {() => {
+        return (
+          <div style={{ flex: 1 }}>
+            <h3>Need support?</h3>
+            <p>
+              Questions? Problems? Need more info? Contact Segment Support for
+              assistance!
+            </p>
+            <button
+              className={styles.visitOurButton}
+              onClick={() => window.open("https://help.moralis.io/", "_blank")}
+            >
+              Visit our Support Page
+            </button>
+          </div>
+        );
+      }}
     </BrowserOnly>
   );
 };
