f98689cedc40db9f0785e6d4dc9f8c134565db1a
Yoseph Kurnia Soenggoro <yosephkurniasoenggoro@gmail.com>
2023-03-28 11:20:41 +0700

    add algolia crawler reindex utils

---
+++ utils/algoliaCrawlerReindex.ts
@@ -0,0 +1,27 @@
+const { config } = require("dotenv");
+
+// inject environment variables
+config();
+
+async function reindex() {
+  try {
+    const BASE64_BASIC_AUTH = `Basic ${Buffer.from(
+      `${process.env.CRAWLER_USER_ID}:${process.env.CRAWLER_API_KEY}`
+    ).toString("base64")}`;
+    fetch(
+      `https://crawler.algolia.com/api/1/crawlers/${process.env.CRAWLER_ID}/reindex`,
+      {
+        method: "POST",
+        headers: {
+          Authorization: BASE64_BASIC_AUTH,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  } catch (e) {
+    const { errors } = e ?? {};
+    console.error(errors);
+  }
+}
+
+reindex();
