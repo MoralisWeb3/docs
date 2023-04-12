b6d00ee622ebefd27354f0b2b22935dfd418fe09
Koen Rijpstra <k.rijpstra@gmail.com>
2023-04-12 14:09:01 +0200

    Fix redirect

--- api/feedback.ts
+++ api/feedback.ts
@@ -3,12 +3,10 @@ import type { VercelRequest, VercelResponse } from "@vercel/node";
 module.exports = async (req: VercelRequest, res: VercelResponse) => {
   // Store it in a database
   const { choice } = req.body;
+  const { page } = req.headers;
+  const ip = req.headers["x-real-ip"] || "undefined";
 
-  console.log(req.headers.referer);
-
-  console.log(req.url);
-
-  console.log(choice);
+  console.log(choice, page, ip);
 
   res.status(201).json({ message: "Success!" });
 };
