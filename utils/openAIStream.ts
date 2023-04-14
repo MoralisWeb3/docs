3bbe4b278f22abd28f4de14c5d50d5b96a021e7a
Yoseph Kurnia Soenggoro <yosephkurniasoenggoro@gmail.com>
2023-04-14 19:38:19 +0700

    fix another open ai wrong env key

--- utils/openAIStream.ts
+++ utils/openAIStream.ts
@@ -13,7 +13,7 @@ export async function OpenAIStream(payload) {
   const res = await fetch("https://api.openai.com/v1/completions", {
     headers: {
       "Content-Type": "application/json",
-      Authorization: `Bearer ${process.env.OPENAI_API_KEY ?? ""}`,
+      Authorization: `Bearer ${process.env.OPENAI_KEY ?? ""}`,
     },
     method: "POST",
     body: JSON.stringify(payload),
