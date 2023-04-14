342bb5e9d6799258fd100a37d4a0da9956272df1
Yoseph Kurnia Soenggoro <yosephkurniasoenggoro@gmail.com>
2023-04-14 23:40:44 +0700

    change model to gpt-3.5-turbo

--- api/gpt-search.ts
+++ api/gpt-search.ts
@@ -22,7 +22,7 @@ const handler = async (req: NextRequest): Promise<Response> => {
     }
 
     const completionOptions = {
-      model: "text-davinci-003",
+      model: "gpt-3.5-turbo",
       prompt,
       max_tokens: 512,
       temperature: 0,
