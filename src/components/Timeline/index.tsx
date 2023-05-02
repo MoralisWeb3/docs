6b7de1af0d6fe5075a143fc055db2d8fd28de593
Koen Rijpstra <k.rijpstra@gmail.com>
2023-05-02 15:17:31 +0200

    Fixed react warnings

--- src/components/Timeline/index.tsx
+++ src/components/Timeline/index.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Link from "@docusaurus/Link";
+import { BiChevronRight } from "react-icons/bi";
 
 const Timeline = ({ content }) => {
   return (
     <ol className="border-l border-neutral-300 dark:border-neutral-500 m-0 p-0 list-none">
-      {content.map((changelog) => {
+      {content.map((changelog, index) => {
         const { date, permalink, title, description } = changelog ?? {};
         return (
-          <li>
+          <li key={index}>
             <div className="flex-start flex items-center pt-3">
               <div className="-ml-[5px] mr-3 h-[9px] w-[9px] rounded-full bg-neutral-300 dark:bg-neutral-500"></div>
               <p className="text-sm text-neutral-500 dark:text-neutral-300 m-0">
@@ -21,7 +22,12 @@ const Timeline = ({ content }) => {
               </p>
             </div>
             <div className="mb-6 ml-4">
-              <Link href={permalink}>Read More {" >"}</Link>
+              <a
+                className="inline-flex justify-center items-center"
+                href={permalink}
+              >
+                Read More <BiChevronRight />
+              </a>
             </div>
           </li>
         );
