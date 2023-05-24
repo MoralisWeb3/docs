3ba63ff31187f0ace875b657ec1d0a81bf0f0fad
Koen Rijpstra <k.rijpstra@gmail.com>
2023-05-24 12:11:40 +0200

    Changed icon to bot

--- src/components/ChatGPT/index.tsx
+++ src/components/ChatGPT/index.tsx
@@ -14,7 +14,7 @@ import {
   AvatarFallback,
   AvatarImage,
 } from "@site/src/components/ui/avatar";
-import { Microscope } from "lucide-react";
+import { Microscope, Bot } from "lucide-react";
 import {
   Alert,
   AlertDescription,
@@ -65,21 +65,7 @@ export default function ChatGPT() {
               </label>
               <div className="relative">
                 <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                  <svg
-                    aria-hidden="true"
-                    className="w-5 h-5 text-gray-500 dark:text-gray-400"
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                    ></path>
-                  </svg>
+                  <Bot className="w-5 h-5 text-gray-500 dark:text-gray-400" />
                 </div>
                 <input
                   type="search"
