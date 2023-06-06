e3306c5ef5fc1dac476b33dbf384c526e59e3fd5
Yoseph Kurnia Soenggoro <yosephkurniasoenggoro@gmail.com>
2023-06-06 17:20:52 +0700

    finish styling for filtering with categories

--- src/components/Guides/index.tsx
+++ src/components/Guides/index.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from "react";
+import React, { useMemo, useState } from "react";
 import { Card } from "@site/src/components/ui/card";
 import { Badge } from "@site/src/components/ui/badge";
 import Link from "@docusaurus/Link";
@@ -6,6 +6,8 @@ import guidesList from "@site/docs/configs/guides/guides.json";
 import ReactMarkdown from "react-markdown";
 
 const Guides = () => {
+  const [selectedCategories, setSelectedCategories] = useState<String | null>();
+
   // Extract All Existing Categories from the Guides List
   const categoriesList = useMemo(() => {
     const allTags = guidesList.map(({ data }) => {
@@ -20,18 +22,33 @@ const Guides = () => {
   }, [guidesList]);
 
   const filteredGuidesList = useMemo(() => {
-    return guidesList.filter(({ data }) => {
-      const { tags } = data ?? {};
-      return tags.includes("NFT API");
-    });
-  }, [guidesList]);
+    return selectedCategories
+      ? guidesList.filter(({ data }) => {
+          const { tags } = data ?? {};
+          return tags.includes(selectedCategories);
+        })
+      : guidesList;
+  }, [guidesList, selectedCategories]);
+
+  const filterGuidesByCategories = (name: String) =>
+    selectedCategories === name
+      ? setSelectedCategories(null)
+      : setSelectedCategories(name);
 
   return (
     <>
       <div className="container">
         <div className="row">
           {categoriesList.map((name) => (
-            <Badge variant="outline" className="cursor-pointer mr-3 mb-3">
+            <Badge
+              variant="outline"
+              className={`cursor-pointer mr-3 mb-3 hover:border-[#57a5ff] hover:text-[#57a5ff] duration-200 ease-in-out${
+                selectedCategories === name
+                  ? " border-[#57a5ff] text-[#57a5ff]"
+                  : ""
+              }`}
+              onClick={() => filterGuidesByCategories(name)}
+            >
               {name}
             </Badge>
           ))}
@@ -47,7 +64,11 @@ const Guides = () => {
                   <h2 className="m-0">{title}</h2>
                   <div>
                     {tags.map((name) => (
-                      <Badge variant="outline" className="cursor-pointer mr-3">
+                      <Badge
+                        variant="outline"
+                        className="cursor-pointer mr-3"
+                        onClick={() => filterGuidesByCategories(name)}
+                      >
                         {name}
                       </Badge>
                     ))}
