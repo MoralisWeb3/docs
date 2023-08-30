c233b5872b6fc04958ec17579dae11da0261f50f
John Versus <johnvs.nagendra@gmail.com>
2023-08-30 15:25:55 +0530

    removed api check

--- api/api-proxy.ts
+++ api/api-proxy.ts
@@ -31,80 +31,40 @@ export default async function (req: VercelRequest, res: VercelResponse) {
       clientIP,
     });
 
-    if (
-      // userAgent?.match(/Mozilla|Chrome|Safari|Edge|Opera/i) && // disabled it temporarly
-      apiMethod === "POST" &&
-      req.body.headers["x-moralis-source"] === "Moralis API docs" &&
-      clientHost === "docs.moralis.io"
-    ) {
-      const newHeaders = {
-        ...headers,
-        "X-API-Key": MORALIS_API_KEY,
-        Authorization: `Bearer ${MORALIS_API_KEY}`,
-        referer: SUPER_SECRET_KEY,
-      };
+    // if (
+    //   // userAgent?.match(/Mozilla|Chrome|Safari|Edge|Opera/i) && // disabled it temporarly
+    //   apiMethod === "POST" &&
+    //   req.body.headers["x-moralis-source"] === "Moralis API docs" &&
+    //   clientHost === "docs.moralis.io"
+    // ) {
+    const newHeaders = {
+      ...headers,
+      "X-API-Key": MORALIS_API_KEY,
+      Authorization: `Bearer ${MORALIS_API_KEY}`,
+      referer: SUPER_SECRET_KEY,
+    };
 
-      const response = await fetch(
-        [
-          hostUrl,
-          path,
-          qs.stringify(query || {}, { addQueryPrefix: true }),
-        ].join(""),
-        {
-          method,
-          headers: newHeaders,
-          body: JSON.stringify(body),
-        }
-      );
-
-      if (response.ok) {
-        const result = await response.json();
-
-        // Try to store the API usage data and catch any errors
-        try {
-          await prisma.apiUsage.create({
-            data: {
-              userAgent,
-              method: apiMethod,
-              hostUrl,
-              path,
-              clientIP,
-              clientHost,
-            },
-          });
-        } catch (dbError) {
-          console.error("Error writing to the database: ", dbError);
-        }
-
-        res.status(response.status).send(result);
-      } else {
-        const error = await response.text();
-
-        // Try to store the API usage data and catch any errors
-        try {
-          await prisma.apiUsage.create({
-            data: {
-              userAgent,
-              method: apiMethod,
-              hostUrl,
-              path,
-              clientIP,
-              clientHost,
-            },
-          });
-        } catch (dbError) {
-          console.error("Error writing to the database: ", dbError);
-        }
-
-        res.status(response.status).send(error);
+    const response = await fetch(
+      [hostUrl, path, qs.stringify(query || {}, { addQueryPrefix: true })].join(
+        ""
+      ),
+      {
+        method,
+        headers: newHeaders,
+        body: JSON.stringify(body),
       }
-    } else {
+    );
+
+    if (response.ok) {
+      const result = await response.json();
+
+      // Try to store the API usage data and catch any errors
       try {
         await prisma.apiUsage.create({
           data: {
             userAgent,
             method: apiMethod,
-            hostUrl: "401 Invalid Key",
+            hostUrl,
             path,
             clientIP,
             clientHost,
@@ -113,8 +73,46 @@ export default async function (req: VercelRequest, res: VercelResponse) {
       } catch (dbError) {
         console.error("Error writing to the database: ", dbError);
       }
-      res.status(401).send({ message: "Invalid Key" });
+
+      res.status(response.status).send(result);
+    } else {
+      const error = await response.text();
+
+      // Try to store the API usage data and catch any errors
+      try {
+        await prisma.apiUsage.create({
+          data: {
+            userAgent,
+            method: apiMethod,
+            hostUrl,
+            path,
+            clientIP,
+            clientHost,
+          },
+        });
+      } catch (dbError) {
+        console.error("Error writing to the database: ", dbError);
+      }
+
+      res.status(response.status).send(error);
     }
+    // } else {
+    //   try {
+    //     await prisma.apiUsage.create({
+    //       data: {
+    //         userAgent,
+    //         method: apiMethod,
+    //         hostUrl: "401 Invalid Key",
+    //         path,
+    //         clientIP,
+    //         clientHost,
+    //       },
+    //     });
+    // } catch (dbError) {
+    //   console.error("Error writing to the database: ", dbError);
+    // }
+    //   res.status(401).send({ message: "Invalid Key" });
+    // }
   } catch (error) {
     res.status(500).send(error);
   } finally {
