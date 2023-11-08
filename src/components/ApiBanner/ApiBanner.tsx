7bd7a6dd704d7cd3160485fe76b9c21d3f07e313
Nazeim <nazemfaour89@gmail.com>
2023-11-08 03:28:12 +0300

    Fix error

--- src/components/ApiBanner/ApiBanner.tsx
+++ src/components/ApiBanner/ApiBanner.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 function ApiBanner() {
   return (
@@ -7,7 +7,14 @@ function ApiBanner() {
         <h1>Don't have an API key yet?</h1>
         <p>Start using this API for your project today.</p>
       </div>
-      <a href="https://admin.moralis.io/register" target="_blank" rel="noopener noreferrer" className="cta-button">Get your free API key</a>
+      <a
+        href="https://admin.moralis.io/register"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="cta-button"
+      >
+        Get your free API key
+      </a>
     </div>
   );
 }
