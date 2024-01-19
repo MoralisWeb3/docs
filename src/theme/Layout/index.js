442bd4e018857474b2e88852823bc6128037fda2
Nazeim <nazemfaour89@gmail.com>
2024-01-19 13:15:01 +0300

    update error syntax

--- src/theme/Layout/index.js
+++ src/theme/Layout/index.js
@@ -1,22 +1,23 @@
-import React from 'react';
-import Layout from '@theme-original/Layout';
-import Head from '@docusaurus/Head';
+import React from "react";
+import Layout from "@theme-original/Layout";
+import Head from "@docusaurus/Head";
 
 export default function LayoutWrapper(props) {
   return (
     <>
       <Head>
         {/* Google Tag Manager */}
-        <script dangerouslySetInnerHTML={{
-          __html: `
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
           (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
           })(window,document,'script','dataLayer','GTM-WD3V6LS6');
           `,
-        }}>
-        </script>
+          }}
+        />
         {/* End Google Tag Manager */}
       </Head>
       {/* Google Tag Manager (noscript) */}
@@ -25,8 +26,8 @@ export default function LayoutWrapper(props) {
           src="https://www.googletagmanager.com/ns.html?id=GTM-WD3V6LS6"
           height="0"
           width="0"
-          style={{ display: 'none', visibility: 'hidden' }}
-        ></iframe>
+          style={{ display: "none", visibility: "hidden" }}
+        />
       </noscript>
       {/* End Google Tag Manager (noscript) */}
       <Layout {...props} />
