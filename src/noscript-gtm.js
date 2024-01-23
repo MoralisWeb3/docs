4f8f74589d667b5ef1df3d6bdb3dbde69c5960bf
Nazeim <nazemfaour89@gmail.com>
2024-01-23 14:16:43 +0300

    update

--- src/noscript-gtm.js
+++ src/noscript-gtm.js
@@ -1,14 +1,14 @@
 // src/noscript-gtm.js
 
-document.addEventListener('DOMContentLoaded', function() {
-  var noscript = document.createElement('noscript');
-  var iframe = document.createElement('iframe');
+document.addEventListener("DOMContentLoaded", function() {
+  var noscript = document.createElement("noscript");
+  var iframe = document.createElement("iframe");
 
-  iframe.src = 'https://www.googletagmanager.com/ns.html?id=GTM-T5T6X6FS';
-  iframe.height = '0';
-  iframe.width = '0';
-  iframe.style.display = 'none';
-  iframe.style.visibility = 'hidden';
+  iframe.src = "https://www.googletagmanager.com/ns.html?id=GTM-T5T6X6FS";
+  iframe.height = "0";
+  iframe.width = "0";
+  iframe.style.display = "none";
+  iframe.style.visibility = "hidden";
 
   noscript.appendChild(iframe);
   document.body.insertBefore(noscript, document.body.firstChild);
