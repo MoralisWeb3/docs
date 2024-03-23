fbcc89e479d8a824532ee216c21199b62bf7ad89
John Versus <johnvs.nagendra@gmail.com>
2024-03-23 16:05:32 +0530

    using redis directly

--- middleware.ts
+++ middleware.ts
@@ -1,29 +1,51 @@
 import { ipAddress, next } from "@vercel/edge";
-import { Ratelimit } from "@upstash/ratelimit";
-import { kv } from "@vercel/kv";
+import Redis from "ioredis";
 
-const ratelimit = new Ratelimit({
-  redis: kv,
-  // 10 requests from the same IP in 10 seconds
-  limiter: Ratelimit.slidingWindow(10, "10 s"),
+const REDIS_URL = process.env.KV_REST_API_URL;
+if (!REDIS_URL) throw new Error("Missing ENV variable");
+const redis = new Redis(REDIS_URL, {
+  tls: {},
 });
 
 export const config = {
   matcher: "/api/api-vercel",
 };
 
+const RATE_LIMIT = 2;
+const WINDOW_SIZE = 10;
+
 export default async function middleware(request: Request) {
-  // Limiting only the specific API path
   const ip = ipAddress(request) || "127.0.0.1";
 
-  console.time("ratelimit-check"); // Start timing before rate limit check
-  const { success } = await ratelimit.limit(ip);
-  console.timeEnd("ratelimit-check"); // End timing after rate limit check
+  try {
+    console.time("ratelimit-check");
+    const currentWindow = Math.floor(Date.now() / 1000 / WINDOW_SIZE);
+    const windowKey = `${ip}-${currentWindow}`;
 
-  // If rate limit is exceeded, respond with a custom error or status code
-  if (!success) {
-    return new Response("Rate limit exceeded", { status: 429 });
+    const pipeline = redis
+      .multi()
+      .incr(windowKey) // Increment the count for the IP in the current window
+      .expire(windowKey, WINDOW_SIZE); // Set expiration for the window key
+
+    const requestCount = await pipeline.exec();
+
+    if (!requestCount) {
+      throw new Error("Redis exec returned null");
+    }
+
+    // Assuming the structure [error, response][] where response is of the shape [null, number]
+    const count = requestCount[0][1] as number;
+    console.log(count);
+    console.timeEnd("ratelimit-check");
+
+    if (count > RATE_LIMIT) {
+      return new Response("Rate limit exceeded", { status: 429 });
+    }
+
+    return next();
+  } catch (error) {
+    console.error("Error in rate limiting:", error);
+
+    return new Response("Internal Server Error", { status: 500 });
   }
-
-  return next();
 }
