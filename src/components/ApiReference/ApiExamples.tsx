d11d8e6e1fece82c26a826548310b59a9eaf76e2
John Versus <johnvs.nagendra@gmail.com>
2024-04-10 20:24:18 +0530

    presisted api value

--- src/components/ApiReference/ApiExamples.tsx
+++ src/components/ApiReference/ApiExamples.tsx
@@ -491,9 +491,13 @@ const ApiExamples = ({
   );
   const formattedValuesForQueryPath = useMemo(() => {
     const { body, path, query } = values ?? {};
+    const pathVal = path;
+    if ((path as any)?.apiKey) {
+      (pathVal as any).apiKey = localStorage.getItem("rpcApiKey");
+    }
     return {
       ...body,
-      ...path,
+      ...pathVal,
       ...query,
     };
   }, [values]);
