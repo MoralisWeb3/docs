3491f2a3ef96e9ee38bb0df6fb9587cc71b5b0e6
iulian <iuliannov1234@gmail.com>
2025-08-22 21:11:04 +0300

    fix

--- src/components/PremiumEndpoint/PremiumEndpoint.tsx
+++ src/components/PremiumEndpoint/PremiumEndpoint.tsx
@@ -1,18 +1,18 @@
 import React from "react";
 import Admonition from "@theme/Admonition";
-
+import ReactMarkdown from "react-markdown";
 interface PremiumEndpointProps {
   customText?: string;
 }
 
-export const PremiumEndpoint: React.FC<PremiumEndpointProps> = ({ 
-  customText 
+export const PremiumEndpoint: React.FC<PremiumEndpointProps> = ({
+  customText,
 }) => {
   const defaultText = `To use this API, you will need an API key associated with a Moralis account on the <strong>Pro</strong> plan or higher.`;
-  
+
   return (
     <Admonition type="info" icon="💡" title="Premium Endpoint">
-      <p dangerouslySetInnerHTML={{ __html: customText || defaultText }} />
+      <ReactMarkdown>{customText || defaultText}</ReactMarkdown>
     </Admonition>
   );
 };
