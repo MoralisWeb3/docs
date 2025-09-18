4e937a81bc0a9dc4694456e570555e092cf6051b
noviulian <iuliannov1234@gmail.com>
2025-09-18 04:47:49 +0300

    remove evmchain method

--- src/components/SupportedChains/EVM/rpc/index.tsx
+++ src/components/SupportedChains/EVM/rpc/index.tsx
@@ -1,33 +1,32 @@
 import React from "react";
-import chainData from "../../../../../configs/api-reference/evmChainData";
+// import chainData from "../../../../../configs/api-reference/evmChainData";
+import chainData from "../../../../../docs/configs/api-reference/evmChainData";
 import GenericTable from "../../Utils/ChainTable";
 import Admonition from "@theme/Admonition";
 
 const headers = [
-  { key: "name", label: "Name" },
-  { key: "type", label: "Type" },
-  { key: "chainID", label: "Chain ID" },
-  { key: "evmChain", label: "EvmChain" },
-  { key: "urls", label: "URLs" },
+    { key: "name", label: "Name" },
+    { key: "type", label: "Type" },
+    { key: "chainID", label: "Chain ID" },
+    // { key: "evmChain", label: "EvmChain" },
+    { key: "urls", label: "URLs" },
 ];
 
 const RpcChainData = () => {
-  const supportedNodes = chainData.filter(
-    (item) => item.rpcNodes && item.rpcNodes.supported
-  );
+    const supportedNodes = chainData.filter((item) => item.rpcNodes && item.rpcNodes.supported);
 
-  return (
-    <div>
-      <Admonition type="tip" title="Supported Chains">
-        <p>
-          See the table below for all supported RPC Nodes. For wider chains we
-          support across all products, check the{" "}
-          <a href="/supported-chains">full list of supported chains.</a>
-        </p>
-      </Admonition>
-      <GenericTable data={supportedNodes} headers={headers} />
-    </div>
-  );
+    return (
+        <div>
+            <Admonition type="tip" title="Supported Chains">
+                <p>
+                    See the table below for all supported RPC Nodes. For wider chains we support
+                    across all products, check the{" "}
+                    <a href="/supported-chains">full list of supported chains.</a>
+                </p>
+            </Admonition>
+            <GenericTable data={supportedNodes} headers={headers} />
+        </div>
+    );
 };
 
 export default RpcChainData;
--- src/components/SupportedChains/EVM/web3api/index.tsx
+++ src/components/SupportedChains/EVM/web3api/index.tsx
@@ -4,29 +4,27 @@ import chainData from "@site/configs/api-reference/evmChainData";
 import Admonition from "@theme/Admonition";
 
 const headers = [
-  { key: "name", label: "Name" },
-  { key: "type", label: "Type" },
-  { key: "chainID", label: "Chain ID" },
-  { key: "evmChain", label: "EvmChain" },
-  { key: "queryParams", label: "<code>chain</code> Query Parameters" },
+    { key: "name", label: "Name" },
+    { key: "type", label: "Type" },
+    { key: "chainID", label: "Chain ID" },
+    // { key: "evmChain", label: "EvmChain" },
+    { key: "queryParams", label: "<code>chain</code> Query Parameters" },
 ];
 
 const EVMChainData = () => {
-  const supportedApiChains = chainData.filter(
-    (item) => item.evmApi && item.evmApi.supported
-  );
+    const supportedApiChains = chainData.filter((item) => item.evmApi && item.evmApi.supported);
 
-  return (
-    <div>
-      <Admonition type="tip" title="Supported Chains">
-        <p>
-          See the table below for all supported RPC Nodes. For wider chains we
-          support across all products, check the{" "}
-          <a href="/supported-chains">full list of supported chains.</a>
-        </p>
-      </Admonition>
-      <GenericTable data={supportedApiChains} headers={headers} />
-    </div>
-  );
+    return (
+        <div>
+            <Admonition type="tip" title="Supported Chains">
+                <p>
+                    See the table below for all supported RPC Nodes. For wider chains we support
+                    across all products, check the{" "}
+                    <a href="/supported-chains">full list of supported chains.</a>
+                </p>
+            </Admonition>
+            <GenericTable data={supportedApiChains} headers={headers} />
+        </div>
+    );
 };
 export default EVMChainData;
